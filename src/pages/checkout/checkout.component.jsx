5507c40b0297fe21a9a748aba42c13d9a792c1c1
Mike <mike.lee816@gmail.com>
2019-12-05 15:27:13 -0500

    Add Stripe payments, and update favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem, {  } from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
@@ -33,6 +34,12 @@ const CheckoutPage = ({ cartItems, total }) => (
         <div className='total'>
             <span>Total: ${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payment*
+            <br/>
+            4242 4242 4242 4242 - Exp: any future date CVV: any 3 digits
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
