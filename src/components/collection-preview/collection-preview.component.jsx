e19378d9bcdd514abeaeaa1366b6dab9d384d055
Mike <mike.lee816@gmail.com>
2019-12-15 14:43:50 -0500

    Move shop data to firestore and add with-spinner higher-order component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -5,15 +5,15 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
+        <h1 className='title'>{title ? title.toUpperCase() : ''}</h1>
         <div className='preview'>
-            {items
+            {items ? items
             .filter((item, idx) => idx < 4)
             .map((item) => (
                 <CollectionItem key={item.id} item={item} />
-            ))}
+            )) : ''}
         </div>
     </div>
-)
+);
 
 export default CollectionPreview;
